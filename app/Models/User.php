2390d8d904e681757a48e4e4c9ea0deda5d41e8f
Ahmed Almabhouh <az540546@gmail.com>
2024-08-25 16:49:41 +0300

    Test

--- app/Models/User.php
+++ app/Models/User.php
@@ -243,7 +243,7 @@ class User extends Authenticatable implements MustVerifyEmail, HasName
 
     public function canAccessPanel(Panel $panel): bool
     {
-        // return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
-        return str_ends_with($this->email, '@hophearts.com') && $this->hasVerifiedEmail();
+        // return str_ends_with($this->email, '@hophearts.com') && $this->hasVerifiedEmail();
+        return true;
     }
 }
